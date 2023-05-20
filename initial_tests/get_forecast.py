71a9c796a24f1eb13a722c7f5a43652f578dd293
= <17607121+Mou141@users.noreply.github.com>
2023-05-20 16:36:24 +0100

    Removed initial_tests directory

--- initial_tests/get_forecast.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def get_site_list(key):
-    r = requests.get(r"http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist", params={"key": key})
-    r.raise_for_status()
-    return r.json()
